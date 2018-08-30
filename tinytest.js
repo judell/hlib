e11fc397751846b9791757e4e5fd8e0e1e44f80b
judell <jon@jonudell.info>
2018-08-30 13:07:18 -0700

    redo testrunner

--- tinytest.js
+++ tinytest.js
@@ -1,101 +1,82 @@
-/**
- * Very simple in-browser unit-test library, with zero deps.
- *
- * Background turns green if all tests pass, otherwise red.
- * View the JavaScript console to see failure reasons.
- *
- * Example:
- *
- *   adder.js (code under test)
- *
- *     function add(a, b) {
- *       return a + b;
- *     }
- *
- *   adder-test.html (tests - just open a browser to see results)
- *
- *     <script src="tinytest.js"></script>
- *     <script src="adder.js"></script>
- *     <script>
- *
- *     tests({
- *
- *       'adds numbers': function() {
- *         eq(6, add(2, 4));
- *         eq(6.6, add(2.6, 4));
- *       },
- *
- *       'subtracts numbers': function() {
- *         eq(-2, add(2, -4));
- *       },
- *
- *     });
- *     </script>
- *
- * That's it. Stop using over complicated frameworks that get in your way.
- *
- * -Joe Walnes
- * MIT License. See https://github.com/joewalnes/jstinytest/
- */
+// inspired by https://github.com/joewalnes/jstinytest
+// promisified for this project
 
 const green = '#99ff99';
 
 const red =  '#ff9999';
 
+let testName
+
+function log(msg) {
+  document.getElementById('log').innerHTML += `<div>${msg}</div>`
+}
+
+function logError(msg) {
+  document.getElementById('log').innerHTML += `<div style="background-color:${red}">${msg}</div>`
+}  
+
 const TinyTest = {
 
-    run: function(tests) {
-        let failures = 0;
-        for (let testName in tests) {
-            let testAction = tests[testName];
-            try {
-                testAction();
-                console.log('Test:', testName, 'OK');
-            } catch (e) {
-                failures++;
-                console.error('Test:', testName, 'FAILED', e);
-                console.error(e.stack);
-            }
-        }
-        setTimeout(function() { // Give document a chance to complete
-            if (window.document && document.body) {
-                document.body.style.backgroundColor = (failures == 0 ? green : red);
-            }
-        }, 0);
-    },
+  run: function(tests) {
 
-    fail: function(msg) {
-        throw new Error('fail(): ' + msg);
-    },
+    log(testName = 'gets token')
+    tests[testName]()
+    .then( () => {
+    log(testName = 'gets "" when no user')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'username input form saves and retrieves value')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'creates facet input form')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'creates default group picklist with > 1 groups when token')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'creates custom group picklist with > 1 groups when token')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'creates group picklist with 1 group when no token')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'creates a pagenote')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'creates an annotation')
+    tests[testName]()
+    .then ( () => {
+    log(testName = 'finds a test annotation')
+    tests[testName]()
+    }) }) }) }) }) }) }) }) }) 
 
-    assert: function(value, msg) {
-        if (!value) {
-            throw new Error('assert(): ' + msg);
-        }
-    },
-
-    assertEquals: function(expected, actual) {
-        if (expected != actual) {
-            throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
-        }
-    },
-
-    assertStrictEquals: function(expected, actual) {
-        if (expected !== actual) {
-            throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
-        }
-    },
-
-    failHard: function() {
-        document.body.style.backgroundColor = red;
+  setTimeout(function() { // Give document a chance to complete
+    if (window.document && document.body) {
+      document.body.style.backgroundColor = green
     }
+  }, 0)
 
+ },
+
+  assert: function(value) {
+    if (!value) {
+      let msg = `${testName}: 'no value'`
+      console.error(msg)
+      logError(msg)
+    }
+  },
+
+  assertEquals: function(expected, actual) {
+    if (expected != actual) {
+      let msg = `${testName}: expected ${expected}, actual ${actual}`
+      console.error(msg)
+      logError(msg)
+    }
+  },
+  
 };
 
-const fail                = TinyTest.fail,
-      assert              = TinyTest.assert,
+const assert              = TinyTest.assert,
       assertEquals        = TinyTest.assertEquals,
       eq                  = TinyTest.assertEquals, // alias for assertEquals
-      assertStrictEquals  = TinyTest.assertStrictEquals,
-      failHard            = TinyTest.failHard,
       tests               = TinyTest.run;
