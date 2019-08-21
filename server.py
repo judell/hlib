bf39aa6a5145e432f0136b70c583e96e8a23d4d0
judell <jon@jonudell.info>
2019-08-21 15:05:48 -0700

    update for python 3

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python2
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-import BaseHTTPServer
+#! /usr/bin/python
+
+import http.server
+from http.server import SimpleHTTPRequestHandler
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
         SimpleHTTPRequestHandler.end_headers(self)
 
+def run(server_class=http.server.HTTPServer,
+        handler_class=CORSRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
 if __name__ == '__main__':
-    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
+    run()
